6641fd203915aae45f581588e04ae2218f50a47b
hibaouafi502-debug <hibaouafi502@gmail.com>
2026-04-13 10:01:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -330,10 +330,10 @@ def get_weather_forecast(lat, lon):
 # INTERFACE PRINCIPALE
 # =========================================================
 st.set_page_config(page_title="PowerRisk", layout="wide")
-st.title("🔐 PowerRisk - Gestion Intelligente des Risques Électriques")
+st.title("PowerRisk - Gestion Intelligente des Risques Électriques")
 col_logo1, col_logo2 = st.columns([1, 5])
 with col_logo1:
-    st.image("Logo.jpg", width=100) 
+    st.image("Logo.jpg", width=150) 
 with col_logo2:
     st.markdown("") 
 
